ffd7f49cd5913d9adb0876a0bbdcbb61a7a46e22
Vansh Mahendra <vanshmahendraofficial@gmail.com>
2022-08-29 12:51:11 +0530

    29-08-2022

--- IntegralsumoptimizedPadding.c
+++ IntegralsumoptimizedPadding.c
@@ -1,6 +1,7 @@
 #include<omp.h>
 #include<stdio.h>
 #define NUM_THREADS 8
+#define pad 8
 
 static long num_steps=100000;
 double step;
@@ -9,7 +10,7 @@ int main()
 {
     int num_threads;
     double pi=0;
-    double sum[NUM_THREADS];
+    double sum[NUM_THREADS][pad];
     omp_set_num_threads(NUM_THREADS);
     
     step=1/(double)num_steps;
@@ -24,17 +25,15 @@ int main()
         {
             num_threads=nthreads;
         }
-        sum[id]=0;
+        sum[id][0]=0;
         for(i=id;i<num_steps;i+=nthreads)
         {
             x=(i+0.5)*step;
-            sum[id]+=4/(1+x*x);
+            sum[id][0]+=4/(1+x*x);
         }
     }
     for(int i=0;i<num_threads;i++)
-    {
-        pi+=sum[i]*step;
-    }
+        pi+=sum[i][0]*step;
     t2=omp_get_wtime();
     printf("pi = %f, time = %f\n",pi,t2-t1);
 }
--- IntegralsumoptimizedWithoutpadding.c
+++ IntegralsumoptimizedWithoutpadding.c
@@ -9,7 +9,6 @@ int main()
 {
     int num_threads;
     double pi=0;
-    double sum[NUM_THREADS];
     omp_set_num_threads(NUM_THREADS);
     
     step=1/(double)num_steps;
@@ -18,22 +17,18 @@ int main()
 
     #pragma omp parallel
     {
-        double x;
+        double sum=0,x;
         int i,id=omp_get_thread_num(),nthreads=omp_get_num_threads();
         if(id==0)
-        {
             num_threads=nthreads;
-        }
-        sum[id]=0;
         for(i=id;i<num_steps;i+=nthreads)
         {
             x=(i+0.5)*step;
-            sum[id]+=4/(1+x*x);
+            sum+=4/(1+x*x);
         }
-    }
-    for(int i=0;i<num_threads;i++)
-    {
-        pi+=sum[i]*step;
+
+        #pragma omp critical
+            pi+=sum*step;
     }
     t2=omp_get_wtime();
     printf("pi = %f, time = %f\n",pi,t2-t1);
